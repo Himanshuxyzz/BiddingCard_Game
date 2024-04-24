33a81d8da5bea187103fa1d12cca849a6e11acba
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-04-24 14:32:57 +0530

    chores: created Forgot password screen - 1 @Himanshu day - 1

--- App/Screens/ForgotPassword/index.jsx
+++ App/Screens/ForgotPassword/index.jsx
@@ -1,13 +1,85 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
 import Colors from "../../Utils/Colors";
 import WhiteText from "../../Components/WhiteText/WhiteText";
 
 const ForgotPassword = () => {
+  const [emailMobile, setEmailMobile] = useState("");
   return (
     <View style={styles.container}>
-      <Text>ForgotPassword</Text>
-      <WhiteText style={{ fontSize: 18 }}>White</WhiteText>
+      <View style={styles.subContainer}>
+        <WhiteText style={styles.forgotPasswordHeading}>
+          Forgot Password
+        </WhiteText>
+        <WhiteText style={styles.subHeading}>
+          Enter Email id/ Mobile number
+        </WhiteText>
+        <TextInput
+          value={emailMobile}
+          //   onChangeText={(text) => setEmailMobile(text)}
+          onChangeText={setEmailMobile}
+          style={styles.input}
+          placeholder="Email id/ Mobile number"
+          keyboardType="default"
+          placeholderTextColor={Colors.INPUT_PLACEHOLDER}
+          autoCapitalize={"none"}
+        />
+        <TouchableOpacity style={styles.btn}>
+          <Text
+            style={{ textAlign: "center", fontSize: 20, color: Colors.PRIMARY }}
+          >
+            Send
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            width: "90%",
+            flexDirection: "row",
+            justifyContent: "flex-end",
+            gap: 8,
+          }}
+        >
+          <Text
+            style={{ color: Colors.WHITE, fontSize: 18, fontWeight: "700" }}
+          >
+            Back to
+          </Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text
+              style={{
+                fontSize: 18,
+                color: "#389AF4",
+                fontWeight: "700",
+                textDecorationLine: "underline",
+              }}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            marginVertical: 20,
+            gap: 10,
+          }}
+        >
+          <View style={{ flex: 1, height: 1, backgroundColor: "gray" }} />
+          <Text style={{ color: Colors.WHITE,fontSize:15 }}>Or</Text>
+          <View style={{ flex: 1, height: 1, backgroundColor: "gray" }} />
+        </View>
+      </View>
     </View>
   );
 };
@@ -17,6 +89,47 @@ export default ForgotPassword;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     backgroundColor: Colors.PRIMARY,
+    padding: 20,
+  },
+  subContainer: {
+    marginTop: "20%",
+    flex: 1,
+    flexDirection: "column",
+    gap: 20,
+    alignItems: "center",
+  },
+  forgotPasswordHeading: {
+    fontSize: 30,
+    fontWeight: "bold",
+    fontFamily: "Laila-Bold",
+  },
+  subHeading: {
+    fontSize: 16,
+    fontFamily: "Inter-Medium",
+  },
+  input: {
+    color: Colors.WHITE,
+    minWidth: "90%",
+    height: 57,
+    marginVertical: 12,
+    padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.1)",
+    fontSize: 18,
+    borderRadius: 10,
+  },
+  btn: {
+    minWidth: "90%",
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: Colors.WHITE,
+    marginTop: 40,
+  },
+  link: {
+    display: "flex",
+    // justifyContent: "center",
+    alignItems: "center",
+    fontSize: 20,
   },
 });
