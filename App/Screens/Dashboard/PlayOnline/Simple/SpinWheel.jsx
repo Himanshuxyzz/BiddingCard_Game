1e54d5135793197f4ac7f9e3eea5c1599aaf5f97
Himanshuxyzz <himanshuxyzz@gmail.com>
2024-05-19 01:33:19 +0530

    chores: fortune wheel spin control from SpinWheel component by making fortuneWheel component reusable @himanshu

--- App/Screens/Dashboard/PlayOnline/Simple/SpinWheel.jsx
+++ App/Screens/Dashboard/PlayOnline/Simple/SpinWheel.jsx
@@ -1,5 +1,5 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
 import WhiteText from "../../../../Components/WhiteText/WhiteText";
 import Header from "../../../../Components/DashboardHeader/Header";
 import FortuneWheel from "../../../../Components/FortuneWheel/FortuneWheel";
@@ -21,10 +21,35 @@ const segmentOptions = [
 
 const SpinWheel = ({ route, navigation }) => {
   const { totalAmount } = route.params;
-  console.log(totalAmount);
+  // console.log(totalAmount);
   const [wheelmessage, setWheelMessage] = useState(
     "Waiting for all the members to join check entries"
   );
+  const [winner, setWinner] = useState(null);
+  const [spinning, setSpinning] = useState(false);
+  const [finished, setFinished] = useState(true); // State to track finished
+  const wheelRef = useRef(null);
+
+  // Function to handle the start of spinning and update the winner
+
+  const handleSpinStart = () => {
+    setWinner(null); // Reset the winner to null when spinning starts
+  };
+
+  // Function to handle the end of spinning and update the winner
+  const handleSpinEnd = (winner) => {
+    setWinner(winner);
+  };
+
+  useEffect(() => {
+    console.log(`IsSpiningStopped - ${finished}`);
+  }, [finished]);
+
+  const spinWheel = () => {
+    if (wheelRef.current) {
+      wheelRef.current.spinWheel();
+    }
+  };
 
   return (
     <View style={styles.container}>
@@ -35,11 +60,34 @@ const SpinWheel = ({ route, navigation }) => {
         >
           {wheelmessage}
         </WhiteText>
+
         {/* Wheel  */}
+
         <FortuneWheel
           totalAmount={totalAmount}
           segmentOptions={segmentOptions}
+          onSpinStart={handleSpinStart} // Pass the spin start handler
+          onSpinEnd={handleSpinEnd} // Pass the function as a prop
+          setFinishedState={setFinished} // Pass setFinished function as prop
+          ref={wheelRef}
         />
+        
+        {/* Conditional rendering based on spinning state */}
+        <View style={{ alignItems: "center", gap: 20 }}>
+          <WhiteText style={{ fontWeight: "600", fontSize: 25 }}>
+            Winner is: {winner}
+          </WhiteText>
+
+          <TouchableOpacity
+            style={[styles.button, !finished && styles.buttonDisabled]}
+            onPress={spinWheel}
+            disabled={!finished}
+          >
+            <Text style={styles.buttonText}>
+              {finished ? "SPIN" : "SPINNING"}
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     </View>
   );
@@ -59,5 +107,18 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: 10,
     gap: 5,
+  },
+  button: {
+    backgroundColor: "blue",
+    padding: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  buttonDisabled: {
+    backgroundColor: "gray",
   },
 });
