ea78e261fe28865e4fca15adcf5cea8926ee624b
Sakshi <93873783+Sakshi0128@users.noreply.github.com>
2024-06-06 17:55:45 +0530

    Added ScrollView in Wallet

--- App/Screens/WalletScreen/WalletMainVerified.jsx
+++ App/Screens/WalletScreen/WalletMainVerified.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import { StyleSheet, View, Text, ScrollView } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { BlurView } from 'expo-blur';
 import GradientVarientOneBtn from "../../Components/Gradient/GradientVariantOneBtn";
@@ -21,40 +21,45 @@ const WalletMainVerified = ({ navigation, route }) => {
   }, [amountAdded, amountDeducted]);
 
   return (
-    <View style={styles.container}>
-      <WalletMainBackground 
-        balance={currentBalance} 
-        onBackPress={() => navigation.goBack()} 
-        amountAdded={amountAdded} 
-        amountDeducted={amountDeducted}
-      />
-      
-      <BlurView intensity={10} tint="light" style={styles.subcontainer}>
-        <View>
-          <GradientVarientOneBtn
-            btnText={"Add Money To Wallet"}
-            style={styles.btn1}
-            onPress={() => navigation.navigate("AddToWallet", { currentBalance })}
-          />
-          <GradientVarientOneBtn
-            btnText={"Withdrawal"}
-            style={styles.btn1}
-            onPress={() => navigation.navigate("Withdraw", { currentBalance })}
-          />
-          <View style={styles.bottom}>
-            <AntDesign name="Safety" size={54} color="green" style={styles.icon} />
-            <WhiteText style={{ fontSize: 24, fontWeight: "bold" }}>Safe & Secure Payments</WhiteText>
-            <WhiteText style={{ fontSize: 15 }}>You can withdraw only 50% from your BC Money Wallet.</WhiteText>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <View style={styles.container}>
+        <WalletMainBackground 
+          balance={currentBalance} 
+          onBackPress={() => navigation.goBack()} 
+          amountAdded={amountAdded} 
+          amountDeducted={amountDeducted}
+        />
+        
+        <BlurView intensity={10} tint="light" style={styles.subcontainer}>
+          <View>
+            <GradientVarientOneBtn
+              btnText={"Add To Wallet"}
+              style={styles.btn1}
+              onPress={() => navigation.navigate("AddToWallet", { currentBalance })}
+            />
+            <GradientVarientOneBtn
+              btnText={"Withdrawal"}
+              style={styles.btn1}
+              onPress={() => navigation.navigate("Withdraw", { currentBalance })}
+            />
+            <View style={styles.bottom}>
+              <AntDesign name="Safety" size={54} color="green" style={styles.icon} />
+              <WhiteText style={{ fontSize: 24, fontWeight: "bold" }}>Safe & Secure Payments</WhiteText>
+              <WhiteText style={{ fontSize: 15 }}>You can withdraw only 50% from your BC Money Wallet.</WhiteText>
+            </View>
           </View>
-        </View>
-      </BlurView>
-    </View>
+        </BlurView>
+      </View>
+    </ScrollView>
   );
 };
 
 export default WalletMainVerified;
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
   container: {
     width: "100%",
     height: "100%",
@@ -66,20 +71,20 @@ const styles = StyleSheet.create({
   subcontainer: {
     width: "100%",
     marginTop: -38,
-    height: "70%",
+    height: "60%",
     padding: 10,
     borderRadius: 4,
     backgroundColor: "#2A2E2E"
   },
   btn1: {
     width: "60%",
-    marginBottom: -30,
-    marginTop: 120,
+    marginBottom: 20,
+    marginTop: 40,
     marginLeft: 80
   },
   bottom: {
     position: "relative",
-    top: 100,
+    top: 50,
     display: "flex",
     flexDirection: "row",
     flexWrap: "wrap",
